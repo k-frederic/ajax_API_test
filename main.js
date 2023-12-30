d1ae2f3c8fbfad8824738424ced198b834517a26
k-frederic <thanxname@outlook.com>
2023-12-30 05:41:09 +0000

    Sort out data presentation in the DOM

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function getData(cb) {
+const baseURL =  " https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
 
     var xhr = new XMLHttpRequest();
     var data;
     
-    xhr.open("GET", " https://ci-swapi.herokuapp.com/api/");
+    xhr.open("GET", baseURL + type + "/");
     xhr.send();
 
     xhr.onreadystatechange = function() {
@@ -13,16 +15,49 @@ function getData(cb) {
     }; 
 };
 
-function printDataToConsole(data) {
-    console.log(data);
+function getTableHeaders(obj) {
+  var tableHeaders = [];
+
+  Object.keys(obj).forEach(function(key) {
+    tableHeaders.push(`<td>${key}</td>`);
+});
+return `<tr>${tableHeaders}</tr>`;
+
 };
 
-getData(printDataToConsole);
- 
+function writeToDocument(type) {
+  var tableRows = [];
+  var el = document.getElementById("data");
+      el.innerHTML = "";
+
+     getData(type, function(data){
+            data = data.results;
+
+           var tableHeaders = getTableHeaders(data[0]);
+
+     data.forEach(function(item) {
+             var dataRow = [];
+
+             Object.keys(item).forEach(function(key) {
+              var rowData = item[key].toString();
+              var truncatedData = rowData.substring(0, 15);
+              dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+       });
+            el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+     });
+          
+  };
+
 
    /* (Codes previously used for demonstration purposes) */
 
  /* 
+   
+     function printDataToConsole(data) {console.log(data);};
+     getData(printDataToConsole);
+ 
 xhr.onreadystatechange = function(){if (this.readyState == 4 && this.status == 200){data = JSON.parse(this.responseText);}};
       
       document.getElementById('data').innerHTML = this.responseText;  
